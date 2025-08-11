5b69ef9ab55ebe13423e651e58bf35e626749692
LeYJa <72301558+LeYJa@users.noreply.github.com>
2025-08-11 02:45:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+import time
 
-# Accede desde variables de entorno
+# 🔐 Obtenemos las credenciales desde los secrets
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 
+print("✅ Script iniciado, generando STRING_SESSION...")
+
 with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print("🔑 STRING_SESSION generado correctamente:\n")
-    print(client.session.save())
+    string_session = client.session.save()
+    print(f"\n🔐 STRING_SESSION generado:\n{string_session}\n")
+
+# ⏳ Mantener vivo el proceso para que Fly.io no lo mate antes de capturar logs
+time.sleep(30)
